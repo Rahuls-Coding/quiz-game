b6852038934fe2be811fe38d735a1f833293df16
Rahul <rahulscodingworld@gmail.com>
2022-03-07 21:28:45 -0500

    fixed front-end

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 function Card() {
   return (
-  <div>
-		<div className="flex justify-center mt-10">
-			<div className="bg-gray-50 dark:bg-slate-900 p-8 rounded-lg">
-				<h1 className="text-center dark:text-white mb-4">Write Todo List</h1>
-				<div className="flex space-x-2 p-2 rounded-md bg-slate-400">
-					<input type="text" placeholder="Write here..." className="w-full dark:text-white bg-slate-400 outline-none" />
-					<button className="bg-green-500 px-2 py-1 rounded-md text-white font-semibold">send</button>
+  <div className="bg-[#131828] text-center grid justify-items-center items-center h-screen">
+	  <div class=" max-w-sm mx-auto overflow-hidden rounded-lg shadow-lg bg-[#30374E]"></div>
+		<div class=" max-w-sm mx-auto overflow-hidden rounded-lg shadow-lg bg-[#30374E]">
+				<div class="px-6 pt-8 py-3">
+					<h1 class=" w-335 h-101 text-xl font-semibold text-gray-800 dark:text-white">When was the industrial revolution born?</h1>
+					<div className = "flex justify-center p-6 ">
+						<input type="text" id="small-input" className="block p-2 h-11 w-70  rounded-lg border  sm:text-md  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Enter your answer..."/>
+					</div>
+					
 				</div>
-			</div>
 		</div>
   </div>
+	
   );
 }
 
