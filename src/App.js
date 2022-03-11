1cd19d7a30f6922af15f77c0690b5dc84aebdb3f
Rahul <rahulscodingworld@gmail.com>
2022-03-11 18:10:15 -0500

    clicking bug

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ class App extends Component {
     super() 
     this.state = {
       search : "",
-      page: "start"
+      page: "start",
+      amount: 0
     }
   }
   onSearched = (event) => {
     this.setState({search: event.target.value})
+    console.log(event.target.value)
+  }
+
+  onClicked = (event) => {
+    this.setState({amount: event.target})
     console.log(event.target.value)
   }
   render() {
@@ -29,12 +35,12 @@ class App extends Component {
             <div className="text-white text-3xl">How many people are playing?</div>
             
             <div className= "flex flex-row space-x-10">
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#DC2323] to-[#ED9B81]"/>
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#FF7455] to-[#FD9E2F]"/>
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-cyan-500 to-blue-500"/>
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#89F4C1] to-[#0BCA08]"/>
+                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#DC2323] to-[#ED9B81] cursor-pointer" clicked = {this.onClicked}/>
+                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#FF7455] to-[#FD9E2F] cursor-pointer" clicked = {this.onClicked}/>
+                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer" clicked = {this.onClicked}/>
+                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#89F4C1] to-[#0BCA08] cursor-pointer" clicked = {this.onClicked}/>
             </div>
-            <div className="text-white text-3xl"></div>
+            <div className="text-white text-3xl">{`Number Of Players Selected: ${this.state.amount}`}</div>
           </div>  
         </div>
         
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import person from "./person.png"
 
-const Profile = ({styling}) => {
+const Profile = ({styling, clicked}) => {
 
     return (
         
             <div className=" max-w-sm mx-auto overflow-hidden rounded-xl drop-shadow-4xl bg-[#30374E] border-2 border-black">
-                <div className={styling}>
+                <div 
+                className={styling}
+                onClick= {() => {
+                    if (clicked >= 4) {
+                        return clicked += 1
+                    } else {
+                        return clicked -= 1
+                    }
+                }}>
                     <div className="rounded overflow-hidden ">
                     <img src={person} alt="Mountain"/>
                 </div>
