4ce843d78db65f9ece5297ce34655abe9547bfee
Rahul <rahulscodingworld@gmail.com>
2022-03-11 18:43:31 -0500

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import Card from './components/Card'
 import Profile from "./components/Profile"
 import React, { Component } from 'react'
 
+
+
 class App extends Component {
   constructor() {
     super() 
     this.state = {
       search : "",
       page: "start",
-      amount: 0
+      count: 0,
+      count1: 0,
+      count2: 0,
+      count3: 0
     }
   }
   onSearched = (event) => {
     this.setState({search: event.target.value})
     console.log(event.target.value)
   }
-
-  onClicked = (event) => {
-    this.setState({amount: event.target})
-    console.log(event.target.value)
-  }
+  
   render() {
     if (this.state.page === "question"){
       return (
@@ -35,12 +36,46 @@ class App extends Component {
             <div className="text-white text-3xl">How many people are playing?</div>
             
             <div className= "flex flex-row space-x-10">
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#DC2323] to-[#ED9B81] cursor-pointer" clicked = {this.onClicked}/>
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#FF7455] to-[#FD9E2F] cursor-pointer" clicked = {this.onClicked}/>
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer" clicked = {this.onClicked}/>
-                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#89F4C1] to-[#0BCA08] cursor-pointer" clicked = {this.onClicked}/>
+                <Profile styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#DC2323] to-[#ED9B81] cursor-pointer" onClicked= {() => {
+                  if (this.state.count < 1) {
+                    this.setState({ count: this.state.count + 1 })
+                  } else {
+                    this.setState({ count: this.state.count - 1 })
+                  }
+                  
+                  }}/>
+                <Profile 
+                styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#FF7455] to-[#FD9E2F] cursor-pointer" 
+                onClicked= {() => {
+                  if (this.state.count1 < 1) {
+                    this.setState({ count1: this.state.count1 + 1 })
+                  } else {
+                    this.setState({ count1: this.state.count1 - 1 })
+                  }
+                  
+                  }}/>
+                <Profile 
+                styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer" 
+                onClicked= {() => {
+                  if (this.state.count2 < 1) {
+                    this.setState({ count2: this.state.count2 + 1 })
+                  } else {
+                    this.setState({ count2: this.state.count2 - 1 })
+                  }
+                  
+                  }}/>
+                <Profile 
+                styling = "px-5 pt-5 pb-5 bg-gradient-to-r from-[#89F4C1] to-[#0BCA08] cursor-pointer" 
+                onClicked= {() => {
+                  if (this.state.count3 < 1) {
+                    this.setState({ count3: this.state.count3 + 1 })
+                  } else {
+                    this.setState({ count3: this.state.count3 - 1 })
+                  }
+                  
+                  }}/>
             </div>
-            <div className="text-white text-3xl">{`Number Of Players Selected: ${this.state.amount}`}</div>
+            <div className="text-white text-3xl">{`Number Of Players Selected: ${this.state.count+this.state.count1+this.state.count2+this.state.count3}`}</div>
           </div>  
         </div>
         
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import person from "./person.png"
 
-const Profile = ({styling, clicked}) => {
+const Profile = ({styling, onClicked}) => {
 
     return (
         
             <div className=" max-w-sm mx-auto overflow-hidden rounded-xl drop-shadow-4xl bg-[#30374E] border-2 border-black">
-                <div 
+                <button 
                 className={styling}
-                onClick= {() => {
-                    if (clicked >= 4) {
-                        return clicked += 1
-                    } else {
-                        return clicked -= 1
-                    }
-                }}>
+                onClick={onClicked}>
                     <div className="rounded overflow-hidden ">
                     <img src={person} alt="Mountain"/>
                 </div>
-                </div>
+                </button>
                 
             </div>
 
